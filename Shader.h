0fa64b9e0b06eb12d6be8927410e4f6525755b74
raimess <rahma.sakti24@gmail.com>
2025-11-28 23:51:46 +0700

    first time upload local project

---
+++ Shader.h
@@ -0,0 +1,28 @@
+#if !defined(SHADER_CLASS_H)
+#define SHADER_CLASS_H
+
+#include <fstream>
+#include <glad/glad.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string get_file_content(string path);
+
+
+class Shader {
+private:
+    GLuint _ID;
+
+public:
+    const GLuint& ID;
+    Shader();
+    Shader(string vertexPath, string fragmentPath);
+    void bind();
+    ~Shader();
+
+    Shader& operator=(Shader&& other) noexcept;
+};
+
+#endif // SHADER_CLASS_H
